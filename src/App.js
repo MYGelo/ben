033a6005a1ce0f0ef3c633f1b2777cf4ba8dc3ce
MYGelo(MiholapOleg) <oleg.miholap95@gmail.com>
2023-10-09 10:42:42 +0300

    1

--- src/App.js
+++ src/App.js
@@ -1,13 +1,23 @@
+import { Routes, Route, Navigate } from 'react-router-dom';
 import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>dfsdfa</p>
-      </header>
-    </div>
+    <Routes>
+      <Route path='/' ></Route>
+    </Routes>
+    // <div className="App">
+    //   <header className="App-header">
+    //     <p>dfsdfa</p>
+    //   </header>
+    // </div>
   );
 }
 
 export default App;
+
+// export const App = () => {
+//   return (
+    
+//   )
+// }
